4763a049c73d6707bc78154e8bd386255a681777
Victor <viktoroperez@gmail.com>
2025-09-11 00:22:18 -0400

    comit 3

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -14,13 +14,11 @@ import {
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "motion/react";
 import Image, { ImageProps } from "next/image";
-import { useOutsideClick } from "@/hooks/use-outside-click";
 
 interface CarouselProps {
   items: React.ReactNode[];   // o React.ReactNode[] si prefieres
   initialScroll?: number;
 }
-type NeonIcon = React.ComponentType<{ className?: string }>;
 
 
 import { StaticImageData } from "next/image";
